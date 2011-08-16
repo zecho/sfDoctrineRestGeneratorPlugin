c63d80cb24c4adf5f2aa695dc61661d09eae0c95
Sébastien HOUZÉ <sebastien.houze@gmail.com>
2011-08-16 19:04:49 +0200

    Start implementation of PRG pattern (Post Redirect Get). When a resource is POSTed a 201 Created status code is returned then a location header redirects to GET ressource route

--- data/generator/sfDoctrineRestGenerator/default/parts/doSave.php
+++ data/generator/sfDoctrineRestGenerator/default/parts/doSave.php
@@ -1,5 +1,33 @@
   protected function doSave()
   {
-    $this->object->save();
-    return sfView::NONE;
+    if($this->object->isNew())
+    {
+      // From Create action
+      $this->object->save();
+
+      <?php $primaryKeys = $this->getPrimaryKeys() ?>
+      $params = array(
+      <?php foreach($primaryKeys as $primaryKey): ?>
+          '<?php echo $primaryKey ?>'  => $this->object-><?php echo $primaryKey ?>,
+      <?php endforeach; ?>
+          'sf_format' => $this->getRequest()->getParameter('sf_format'),
+      );
+
+
+      //$url = $this->generateUrl('customer_show', $params);
+      //$this->redirect($url, 201);
+
+      $this->getRequest()->getParameterHolder()->clear();
+      $this->getRequest()->getParameterHolder()->add($params);
+      $this->getRequest()->setMethod(sfWebRequest::GET);
+
+      $this->getResponse()->setStatusCode(201);
+      $this->forward('<?php echo strtolower($this->getModelClass()) ?>', 'show');
+    }
+    else
+    {
+      // From Update action
+      $this->object->save();
+      return sfView::NONE;
+    }
   }
