bb6de7ccd0f1cf636174e1905e170a0ebb976593
Sébastien HOUZÉ <sebastien.houze@gmail.com>
2011-10-19 20:01:52 +0200

    PRG pattern modification : forward based on module name, not model class. Plus deleting some commented test code

--- data/generator/sfDoctrineRestGenerator/default/parts/doSave.php
+++ data/generator/sfDoctrineRestGenerator/default/parts/doSave.php
@@ -13,16 +13,12 @@
           'sf_format' => $this->getRequest()->getParameter('sf_format'),
       );
 
-
-      //$url = $this->generateUrl('customer_show', $params);
-      //$this->redirect($url, 201);
-
       $this->getRequest()->getParameterHolder()->clear();
       $this->getRequest()->getParameterHolder()->add($params);
       $this->getRequest()->setMethod(sfWebRequest::GET);
 
       $this->getResponse()->setStatusCode(201);
-      $this->forward('<?php echo strtolower($this->getModelClass()) ?>', 'show');
+      $this->forward($this->getModuleName(), 'show');
     }
     else
     {
